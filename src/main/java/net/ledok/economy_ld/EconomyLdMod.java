ac8b97e6a03121068ea7a4d4f0b5f54f1d94cae0
ledok <supermaster.ua.lviv@gmail.com>
2026-05-08 18:53:00 +0200

    First commit.

---
+++ src/main/java/net/ledok/economy_ld/EconomyLdMod.java
@@ -0,0 +1,10 @@
+package net.ledok.economy_ld;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EconomyLdMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/ledok/economy_ld/client/EconomyLdModClient.java
@@ -0,0 +1,10 @@
+package net.ledok.economy_ld.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EconomyLdModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
