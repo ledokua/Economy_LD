ef4c750f859ef8ce0be33e07344d83724c1f0116
ledok <supermaster.ua.lviv@gmail.com>
2026-05-08 19:46:03 +0200

    Revert "First implementation."
    This reverts commit 354a90f7b361d436a8c0ff0dc0eb76e095a92c7a.

--- src/main/java/net/ledok/economy_ld/EconomyLdMod.java
+++ src/main/java/net/ledok/economy_ld/EconomyLdMod.java
@@ -1,21 +1,10 @@
 package net.ledok.economy_ld;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.ledok.economy_ld.config.ConfigLoader;
-import net.ledok.economy_ld.config.EconomyConfig;
-import net.ledok.economy_ld.manager.EconomyManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EconomyLdMod implements ModInitializer {
-    public static final String MOD_ID = "economy_ld";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        EconomyConfig config = ConfigLoader.load(LOGGER);
-        EconomyManager.getInstance().start(config, LOGGER);
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> EconomyManager.getInstance().stop());
     }
 }
